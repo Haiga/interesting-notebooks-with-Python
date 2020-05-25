a248a3737104b9ab061f03eb3e4f7513b3cf0eb9
pedro_rga <pedro_rga@hotmail.com>
2020-05-25 11:32:50 -0300

    Add example of usage bar 3d for hist or surface

---
+++ Visualização de Dados/bar-three-dimensional.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+fig = plt.figure()  # figsize=(4, 4)
+ax = fig.add_subplot(111, projection='3d')
+
+# Y é a lateral
+# X é a frente
+# Z é a altrura
+# ax.bar3d(xpos, ypos, zpos, tam_fronte, tam_lateral, altura_barra, zsort='average')
+ax.bar3d(1, 1, 1, 1, 2, 2, zsort='average')
+ax.bar3d(3, 1, 1, 1, 2, 4, zsort='average')
+ax.bar3d(5, 1, 1, 1, 2, 3, zsort='average')
+
+# ax.set_zlim(-10, 10)
+# ax.set_xlim(-10, 10)
+ax.set_ylim(0, 10)
+
+# ax = fig.add_axes(MyAxes3D(ax, 'l'))
+# ax = fig.add_axes(MyAxes3D(ax, 'lr'))
+
+# Coloca os ticks do z no canto direito
+ax.zaxis._axinfo['juggled'] = (1, 2, 0)
+# ax.xaxis._axinfo['juggled'] = (2,0,1)
+
+# Remove os ticks do y na lateral esquerda
+ax.yaxis.set_ticks([])
+plt.show()
